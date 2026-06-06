c89aa9d6c8d63c8716c600f63464f8aaccda78c1
ExtraBurrito <nikishterr@gmail.com>
2026-06-06 20:29:59 +0300

    I wrote the storage logic for language and settings (DataStore)

--- app/src/main/java/com/example/mobileapps2025/data/UserPreferencesRepository.kt
+++ app/src/main/java/com/example/mobileapps2025/data/UserPreferencesRepository.kt
@@ -1,4 +1,24 @@
 package com.example.mobileapps2025.data
 
-class UserPreferencesRepository {
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+// Create the datastore file, it will be called "user_prefs"...
+private val Context.dataStore by preferencesDataStore(name = "user_prefs")
+
+class UserPreferencesRepository(private val context: Context) {
+    companion object {
+        val LANGUAGE_KEY = stringPreferencesKey("app_language")
+    }
+    // English by default if NULL
+    val currentLanguageFlow: Flow<String> = context.dataStore.data.map { preferences ->  preferences[LANGUAGE_KEY] ?: "English"
+    }
+    suspend fun saveLanguage(language: String) {
+        context.dataStore.edit { preferences -> preferences[LANGUAGE_KEY] = language
+        }
+    }
 }
